abb19df509be92677cc728276490cfcb188a6e1e
sdfs <sdfdf@gamil.com>
2025-05-07 15:01:18 -0500

    conexion

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const parser = require("body-parser");
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+require('dotenv').config();
+app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
+app.use(parser.json()); // transforma los datos a formato JSON
+
+//Conexión a la base de datos
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conexión exitosa"))
+    .catch((error) => console.log(error));
+//Conexión al puerto
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
