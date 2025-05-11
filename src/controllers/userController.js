7eb7fa58ff1074abedb6bbd1a694a0fa71e6909c
Jasmin <Jasminmuneva@gmail.com>
2025-05-10 23:26:12 -0500

    implementar inicio de sesión

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,3 +16,19 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: 'Error al registrar usuario.', error });
   }
 };
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return res.status(404).json({ message: 'Usuario no encontrado.' });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(401).json({ message: 'Contraseña incorrecta.' });
+  
+      res.status(200).json({ message: 'Inicio de sesión exitoso', user });
+    } catch (error) {
+      res.status(500).json({ message: 'Error al iniciar sesión.', error });
+    }
+  };
+  
